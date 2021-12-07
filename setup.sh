548a1b828b56e7aea9b876ed7093d55e6955d16a
Francisco-BT <pancho10810@gmail.com>
2021-12-07 16:47:42 -0600

    Fix some setup.sh commands

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ echo "Instaling git..."
 sudo apt install git
 
 echo "Installig zsh..."
-sudo apt-get install zsh
+sudo apt install zsh
 
 echo "Set up zsh as default terminal"
 chsh -s `which zsh`
@@ -11,28 +11,28 @@ echo "Make sure the system has curl"
 sudo apt install curl
 
 echo "Make sure kitty is installed"
-sudo pat install kitty
+sudo apt install kitty
+
+echo "Installing neovim"
+sudo apt install neovim
 
 echo "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+echo "Adding zsh plugins"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-echo "Add to you .zsrhc file"
-
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-echo "Add \"zsh-autosuggestions\" to your .zshrc file"
-
 git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
-echo "Add zsh-nvm on your .zshrc"
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
+echo "Adding fzf to search files"
+echo "You can search files using Ctrl + T"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-echo "You can search files using Ctrl + T"
-echo "\n Set up vim and zsh config"
 
+echo "\n Set up vim and zsh config"
 cp .vimrc ~/.vimrc
 cp .zshrc ~/.zshrc
 cp .tmux.conf ~/.tmux.conf
@@ -53,18 +53,13 @@ echo "Setup vimplug"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-
-echo "Finished setup. You can use https://github.com/denysdovhan/spaceship-prompt as your oh-my-zsh theme"
 echo "Run :PlugInstall when you executed vim"
-
 echo "Add git utils alias"
 git config --global alias.st status
 git config --global alias.ps push
 git config --global alias.ch checkout
 git config --global alias.pl pull
-
-echo "Installing neovim"
-sudo apt install neovim
+git config --global alias.cm commit
 
 echo "Creating confguration between vim and neovim"
 

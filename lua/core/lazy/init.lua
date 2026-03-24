2c9b9d533e3875f3c863c2c33d37d7be729ddcca
Francisco-BT <pancho10810@gmail.com>
2026-03-24 03:38:43 -0600

    feat: add more remaps to be a better person

--- lua/core/lazy/init.lua
+++ lua/core/lazy/init.lua
@@ -4,4 +4,5 @@ return {
     "nvim-lua/plenary.nvim",
     name = "plenary"
   },
+  "eandrju/cellular-automaton.nvim",
 }
--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -1,24 +1,28 @@
 local keymap = vim.keymap.set
 
 -- Netrw vertical split
-keymap("n", "<leader>pv", "<cmd>Vex<cr>", { desc = "Vertical file explorer (netrw)" })
+keymap("n", "<leader>pv", ":Ex<CR>", { desc = "Vertical file explorer (netrw)" })
 
 -- Quickfix navigation
+keymap("n", "<leader>k", "<cmd>lnext<CR>zz", { desc = "Next location list item (centered)" })
+keymap("n", "<leader>j", "<cmd>lprev<CR>zz", { desc = "Previous location list item (centered)" })
 keymap("n", "<C-j>", "<cmd>cnext<cr>zz", { desc = "Quickfix next" })
 keymap("n", "<C-k>", "<cmd>cprev<cr>zz", { desc = "Quickfix previous" })
 keymap("n", "<C-d>", "<C-d>zz", { desc = "Jump down half page and center" })
 keymap("n", "<C-u>", "<C-u>zz", { desc = "Jump up half page and center" })
+keymap("n", "n", "nzzzv", { desc = "Next search result (centered)" })
+keymap("n", "N", "Nzzzv", { desc = "Previous search result (centered)" })
 
 -- Yank
 keymap("x", "<leader>p", '"_dP', { desc = "Paste without overwriting register" })
 keymap("x", "<leader>y", '"+y', { desc = "Yank selection to system clipboard" })
 keymap("n", "<leader>y", '"+y', { desc = "Yank to system clipboard" })
 keymap("n", "<leader>Y", 'gg"+yG', { desc = "Yank entire buffer to clipboard" })
+keymap({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete to void register" })
 
 -- Move selection
 keymap("x", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
 keymap("x", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
-
 
 
 -- Replace
@@ -27,3 +31,11 @@ keymap("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
 -- Helpers
 keymap("n", "J", "mzJ`z", { desc = "Join lines without moving cursor" })
 keymap("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Make current file executable" })
+
+-- Static formatting
+keymap("n", "=ap", "ma=ap'a", { desc = "Format paragraph keeping cursor position" })
+
+-- Just kidding
+keymap("n", "<leader>ca", function()
+    require("cellular-automaton").start_animation("make_it_rain")
+end, { desc = "Cellular Automaton: Make it rain!" })
